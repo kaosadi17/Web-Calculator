7f3642999d8985cf6006d04cc70ffbfb76239d4b
Amali Osadi <kaosadi17@gmail.com>
2020-01-21 13:38:53 +0530

    Add number related logic

--- src/script.js
+++ src/script.js
@@ -28,3 +28,14 @@ function getFormattedNumber(num) {
 function reverseNumberFormat(num) {
     return Number(num.replace(/,/g, ''));
 }
+
+var number = document.getElementsByClassName("number");
+for (var i=0; i<number.length;i++){
+    number[i].addEventListener('click',function(){
+        var output = reverseNumberFormat(getOutput());
+        if (output != NaN){
+            output = output + this.id;
+            printOutput(output);
+        }
+    })
+}
